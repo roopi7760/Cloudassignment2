6eb7fc965f3cfd83caf9108983cf10758ae64cfa
Roopesh kumar <kroopeshkumar@yahoo.com>
2016-01-28 02:08:21 -0600

    Done
    Done with almost everything except version control

--- app.py
+++ app.py
@@ -9,17 +9,18 @@ import couchdb
 import datetime
 import hashlib
 import time
+import json
 
 #variables for Cloudant Database
 USERNAME = "7d79e7d0-6d67-4a21-aac0-15fff940c492-bluemix"
 PASSWORD = "4828714fecff497426fe357632f22a10f6d0a82e2d0f3888ea59bbad2a57e5fb"
 
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 #connect to the Cloudant Database instance
 couch = couchdb.Server("https://7d79e7d0-6d67-4a21-aac0-15fff940c492-bluemix:4828714fecff497426fe357632f22a10f6d0a82e2d0f3888ea59bbad2a57e5fb@7d79e7d0-6d67-4a21-aac0-15fff940c492-bluemix.cloudant.com")
 db = couch['appsync']
-
-UPLOAD_FOLDER = './uploads'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -28,57 +29,65 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/click/<objectname>')
-def click_here(objectname):
-    return  objectname
-
-@app.route('/container/<filename>')
+@app.route('/list/<filename>')
 def downloadFile(filename):
     downdoc = db.get(filename)
     file = open(UPLOAD_FOLDER + '/' + filename,"w+")
     file.write(downdoc['contents'])
     file.close()
-    response = make_response()
-    response.headers["Content-Disposition"] = ""\
-        "attachment; filename=%s" % UPLOAD_FOLDER + "/" +filename
-    return response
-    #return send_from_directory(directory=app.config['UPLOAD_FOLDER'], filename=filename)
+    return send_from_directory(directory=app.config['UPLOAD_FOLDER'], filename=filename)
 
 
-@app.route('/container')
+@app.route('/list')
 def getContainers():
- return render_template('hello.html', db=db)
+ return render_template('Filelist.html', db=db)
 
 @app.route('/', methods = ['POST','GET'])
 def index():
     if request.method == 'POST':
-        if request.form['submit'] == 'Upload':
+        if request.form['submit'] == 'Upload File':
             return redirect(url_for('upload_file'))
-        elif request.form['submit'] == 'click here':
+        elif request.form['submit'] == 'View Files':
             return redirect(url_for('getContainers'))
     return '''
-    <!doctype html>
-        <title>Welcome!</title>
-        <h1>Welcome to Cloud computing!</h1>
-        <form action="" method=post enctype=multipart/form-data>
-        <input type='submit' name='submit' value='click here'>
-        <input type='submit' name='submit' value='Upload'> </form>
-        '''
+    <!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+</head>
+<body>
+<h1 align="center">Welcome to Cloud computing!</h1>
+<h3 align="center">File storage service by BlueMix</h3><br/><br/>
+<table width="40%" align="center">
+<form action="" method=post enctype=multipart/form-data>
+        <tr align="center">
+            <th ><input type='submit' name='submit' value='View Files'></th>
+            <th > <input type='submit' name='submit' value='Upload File' ></th>
+        </tr>
+
+</form>
+</table>
+</body>'''
 
 @app.route('/uploading/<filename>')
 def insertFileIntoDB(filename):
-    #read the file and store the contents
     file = open(UPLOAD_FOLDER + "/" + filename, "rb")
     content = file.read()
     dateModified = time.strftime("%m/%d/%Y %I:%M:%S %p",time.localtime(os.stat(UPLOAD_FOLDER + "/" + filename).st_mtime))
-    #dateModified = os.path.getmtime(filename)
-    #generate the hash code for the contents of the file
     filehash = hashlib.sha224(content).hexdigest()
-    #acess the db to check whether it has document for same file name
+
     doc = db.get(filename)
     if doc:
         if doc['hashcode'] == filehash:
-            return "File Exist on the server"
+            message = 'Sorry! File already exist on the server'
+            return render_template('info.html',message=message)
+        else:
+            doc['contents'] = content
+            doc['hashcode'] = filehash
+            doc['datemodified'] = dateModified
+            db.save(doc)
+            message = "File Update Successfully"
+            return render_template('info.html', message = message)
 
     docid,docrev = db.save({
         '_id' : filename,
@@ -87,7 +96,8 @@ def insertFileIntoDB(filename):
         'datemodified' : dateModified
     })
     doc = db.get(docid)
-    return doc['datemodified']
+    message = "File uploaded Successfully"
+    return render_template('info.html',message=message)
 
 
 @app.route('/upload', methods=['GET', 'POST'])
@@ -98,8 +108,10 @@ def upload_file():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             file.close()
-        return redirect(url_for('insertFileIntoDB',filename = filename))
-
+            return redirect(url_for('insertFileIntoDB',filename = filename))
+        else:
+            message = "Sorry! Either File is not supported or file not selected"
+            return render_template('info.html',message=message)
     return '''
     <!doctype html>
     <title>Upload new File</title>
@@ -108,19 +120,16 @@ def upload_file():
       <p><input type=file name=file>
          <input type=submit value=Upload>
     </form>
+    <a href='/'> << Back </a>
     '''
+
 @app.route('/delete/<filename>')
 def deleteFile(filename):
     deldocs = db.get(filename)
     res = db.delete(deldocs)
-    return '''
-    <!doctype html>
-    <p>File deleted</p>
-    <a href = '/'>Home</a>'''
+    message = "File deleted!"
+    return render_template('info.html',message=message)
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return filename
 
 
 if __name__ == '__main__':
